7413eb56c6cc0024fa0ab0f7ef953c5be0ad50b9
dmb-2222 <dmb-2222@yandex.ru>
2020-05-26 22:50:12 +0300

    Updates

---
+++ precache-manifest.fb4f2f22257df0823596db77f5b0f930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b92764f02c37d95d6a21239ec6b60d9",
+    "url": "/codete/index.html"
+  },
+  {
+    "revision": "1b08630f64429d5dd8fc",
+    "url": "/codete/static/css/main.7ed0e446.chunk.css"
+  },
+  {
+    "revision": "d06b880055023a5ee696",
+    "url": "/codete/static/js/2.caba32ac.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/codete/static/js/2.caba32ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b08630f64429d5dd8fc",
+    "url": "/codete/static/js/main.f4a7276e.chunk.js"
+  },
+  {
+    "revision": "5631dfc1d01fbaf318d8",
+    "url": "/codete/static/js/runtime-main.d903d46e.js"
+  }
+]);
